98ec81f9e7386cca1aa1bc301d353ac1ae1ba043
feelinglight <kolpacc123@mail.ru>
2024-03-16 17:38:41 +0500

    Добавлен docker-клиент бареос

--- bareos/docker/build.sh
+++ bareos/docker/build.sh
@@ -4,5 +4,5 @@
 docker build -t bar-dir --target bareos-dir .
 docker build -t bar-web --target bareos-webui .
 docker build -t bar-sd --target bareos-sd .
-# docker build -t bar-fd --target bareos-fd .
+docker build -t bar-fd --target bareos-fd .
 
