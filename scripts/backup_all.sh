3f1107f193c5e56da0c087f59309b61c762d03da
feelinglight <kolpacc123@mail.ru>
2024-11-24 13:20:25 +0000

    Добавлены бэкапы сервисов

--- scripts/backup_all.sh
+++ scripts/backup_all.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+services_dir="$script_dir/../services"
+
 echo "Backup started"
 
 echo "Backup pihole started"
@@ -16,16 +19,24 @@ docker exec -t gitea bash -c 'sqlite3 /data/gitea/gitea.db ".backup /backup/gite
 echo "Backup gitea finished"
 
 
-# echo "Backup calibre started"
-# docker exec -t calibre-web bash -c 'rm -rf /backup/*'
-# docker exec -t calibre-web bash -c 'sqlite3 /data/gitea/gitea.db ".backup /backup/gitea.db"'
-# echo "Backup calibre finished"
+echo "Backup calibre started"
+rm -rf "${services_dir}/calibre-web/backup/*"
+sqlite3 "${services_dir}/calibre-web/data/app.db" ".backup ${services_dir}/calibre-web/backup/app.db"
+sqlite3 "${services_dir}/calibre-web/data/gdrive.db" ".backup ${services_dir}/calibre-web/backup/gdrive.db"
+sqlite3 "${services_dir}/calibre-web/data/metadata.db" ".backup ${services_dir}/calibre-web/backup/metadata.db"
+echo "Backup calibre finished"
 
 
 echo "Backup postgres started"
 docker exec -t postgres-db bash -c 'rm -rf /backup/*'
 docker exec -t postgres-db bash -c 'pg_dumpall -U postgres > "/backup/postgres_dump.sql"'
 echo "Backup postgres finished"
+
+
+echo "Backup mariadb started"
+docker exec -t mariadb bash -c 'rm -rf /backup/*'
+docker exec -t mariadb bash -c 'mariadb-dump --user=root --password="${MARIADB_ROOT_PASSWORD}" --all-databases > "/backup/mariadb_dump.sql"'
+echo "Backup mariadb finished"
 
 
 echo "Backup finished"
