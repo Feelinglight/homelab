9c78ff764a072d9438a368bdebd7a31332bc58b6
feelinglight <kolpacc123@mail.ru>
2025-08-03 16:29:49 +0000

    Исправлены папки bareos

--- services/bareos/docker/sd-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-/scripts/make_bareos_config.sh /etc_bareos /etc/bareos
-
-# Run Dockerfile CMD
-exec "$@"
--- services/bareos/docker/webui-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-/scripts/make_bareos_config.sh /etc_bareos /etc/bareos-webui
-
-php-fpm8.1
-
-# Run Dockerfile CMD
-exec "$@"
